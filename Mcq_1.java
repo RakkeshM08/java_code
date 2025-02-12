2e0e649606f66a3f2b66b557a89cbc22b97dfd3b
RakkeshM08 <rakkeshmurugesan@gmail.com>
2025-02-12 16:04:23 +0530

    Add files via upload

---
+++ Mcq_1.java
@@ -0,0 +1,17 @@
+class Mcq_1
+{
+    public static void main (String s[])
+    {
+        int a=86;
+        int b=94;
+        if(true)
+            System.out.println("a"+"b");
+        else    
+            System.out.println(a+b);
+        System.out.println("a"+b);
+    }
+}
+/*
+ab
+a94
+*/
