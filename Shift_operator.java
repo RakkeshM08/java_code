f732ec8c7223cedeabd64afe7cbd6dd69157be27
RakkeshM08 <rakkeshmurugesan@gmail.com>
2025-02-12 14:13:24 +0530

    Add files via upload

---
+++ Shift_operator.java
@@ -0,0 +1,13 @@
+class Shift_operator
+{
+    public static void main (String s[])
+    {
+        int a=10;
+        a=a<<2;
+        System.out.println("leftShift :"+a);
+        int b=10;
+        b=b>>2;
+        System.out.println("RightShift :"+b);
+
+    }
+}
