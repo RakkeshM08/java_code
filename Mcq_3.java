c81e16ace756e098372e64f42098f625f3182848
RakkeshM08 <rakkeshmurugesan@gmail.com>
2025-02-12 16:11:10 +0530

    Add files via upload

---
+++ Mcq_3.java
@@ -0,0 +1,17 @@
+class Mcq_3
+{
+    public static void main (String s[])
+    {
+       int i=1;
+       while(i>0)
+       {
+           i=1;
+           System.out.println(i--);
+           i--;
+           
+       }
+    }
+}
+/*
+1
+*/
