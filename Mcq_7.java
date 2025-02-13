347b364f34f74342bdee59aec024a8b9622da546
RakkeshM08 <rakkeshmurugesan@gmail.com>
2025-02-13 12:09:55 +0530

    Add files via upload

---
+++ Mcq_7.java
@@ -0,0 +1,30 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+public class Mcq_7
+{
+	public static void main(String[] args) {
+	   int n=5;int k=1;
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=1;j<i;j++)
+        {
+            System.out.print(k+" ");
+            k++;
+        }
+        System.out.println();
+    }
+    
+	}
+}
+/*
+1 
+2 3 
+4 5 6 
+7 8 9 10 
+*/
