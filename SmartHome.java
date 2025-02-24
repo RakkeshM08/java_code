6118c7f0b550bc054695700edb5d0ee855bb9c19
RakkeshM08 <rakkeshmurugesan@gmail.com>
2025-02-24 16:28:06 +0530

    Add files via upload

---
+++ SmartHome.java
@@ -0,0 +1,53 @@
+class SmartHome {
+
+    public static final class Device {
+        private final int id;
+        private final String name;
+        public static final int MAX_DEVICES = 50;
+
+        public Device(int id, String name) {
+            this.id = id;
+            this.name = name;
+        }
+
+        public final String getDeviceInfo() {
+            return "Device ID: " + id + ", Device Name: " + name;
+        }
+    }
+
+    public final static class Configuration {
+        public static final int MAX_POWER_LIMIT = 5000;
+    }
+
+    public abstract static class DeviceType {
+        public abstract String getDeviceCategory();
+    }
+
+    public static class LightDevice extends DeviceType {
+        
+        public String getDeviceCategory() {
+            return "Lighting Device";
+        }
+    }
+
+    public static class FanDevice extends DeviceType {
+        
+        public String getDeviceCategory() {
+            return "Fan Device";
+        }
+    }
+
+    public static void main(String[] args) {
+        Device device = new Device(1, "Smart Bulb");
+        System.out.println(device.getDeviceInfo());
+
+        System.out.println("Max Power Limit: " + Configuration.MAX_POWER_LIMIT);
+
+        DeviceType light = new LightDevice();
+        DeviceType fan = new FanDevice();
+
+        System.out.println("Light Device Category: " + light.getDeviceCategory());
+        System.out.println("Fan Device Category: " + fan.getDeviceCategory());
+    }
+}
+                         
